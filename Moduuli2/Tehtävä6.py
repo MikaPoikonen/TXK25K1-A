63878bae0be5983ea530853bd82ac0e522f59d92
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-17 11:22:57 +0200

    Moduuli 2 harjoitus 6

---
+++ Moduuli2/Tehtävä6.py
@@ -0,0 +1,12 @@
+import secrets
+random_integer1 =secrets.randbelow(10)+1
+random_integer2 =secrets.randbelow(10)+1
+random_integer3 =secrets.randbelow(10)+1
+print (random_integer1,random_integer2,random_integer3)
+
+
+random_integer1 = secrets.randbelow(6)+1
+random_integer2 = secrets.randbelow(6)+1
+random_integer3 = secrets.randbelow(6)+1
+random_integer4 = secrets.randbelow(6)+1
+print (random_integer1,random_integer2,random_integer3,random_integer4)
