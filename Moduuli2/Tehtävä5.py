5e9b4157c729986169c99323e482a03729fa49d1
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-17 11:06:40 +0200

    Moduuli 2 harjoitus 5

---
+++ Moduuli2/Tehtävä5.py
@@ -0,0 +1,12 @@
+luku1=input("Anna leiviskä: ")
+luku2=input("Anna naulat:")
+luku3=input("Anna luodit: ")
+leiviska_int =int(luku1)*20*32*0.0133
+naula_int=int(luku2)*32*0.0133
+luoti_float=int(luku3)*0.0133
+kokonaispaino= float(leiviska_int+naula_int+luoti_float)
+
+kokonaisluku =int(kokonaispaino)
+desimaali=(kokonaispaino- kokonaisluku)*1000
+print(f"Kiloa: {kokonaisluku}")
+print(f"Grammat: {desimaali:.2f}")
