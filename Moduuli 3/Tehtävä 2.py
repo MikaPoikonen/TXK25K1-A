27089b68ee804a3ceba27a34b929bdc73ad24c2e
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-20 13:55:58 +0200

    Tehtävä 2 moduuli 3

---
+++ Moduuli 3/Tehtävä 2.py
@@ -0,0 +1,8 @@
+print ("Anna hyttiluokka LUX,A,B tai C")
+hytti = input (str ("hyttiluokka"))
+if "LUX" in hytti: (print("LUX on parvekkeellinen hytti yläkannella.!"))
+elif "A" in hytti: (print("A on ikkunallinen hytti autokannen yläpuolella."))
+elif "B" in hytti: (print("B on ikkunaton hytti autokannen yläpuolella."))
+elif "C" in hytti: (print("C on ikkunaton hytti autokannen alapuolella."))
+else: (print("Virheellinen hyttiluokka."))
+
