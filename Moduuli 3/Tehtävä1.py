e30c578524f34d6880d855356c9ec9f56e81c32f
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-20 11:35:08 +0200

    Moduuli 3 harjoitus 1

---
+++ Moduuli 3/Tehtävä1.py
@@ -0,0 +1,6 @@
+pituus =float (input ("Anna kuhan pituus"))
+alamitta = str (36-pituus)
+if pituus <=36:
+    (print ("senttiä alimmasta sallitusta pyyntimitasta puuttuu: " + str (alamitta)))
+if pituus >=36: str (print("Kuha on sallitun pituinen"))
+
