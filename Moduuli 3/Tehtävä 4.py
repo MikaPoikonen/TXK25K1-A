b0ddb59b21a4a2f75deb295f7e5f78f5f6827c84
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-21 09:35:13 +0200

    Moduuli 3 Tehtävä 4

---
+++ Moduuli 3/Tehtävä 4.py
@@ -0,0 +1,6 @@
+vuosiluku =float (input("Anna vuosiluku: "))
+if (vuosiluku % 4 == 0 and vuosiluku % 100 !=0) or vuosiluku % 400 == 0: print("on karkausvuosi")
+else: print("Vuosi ei ole karkausvuosi")
+
+
+
