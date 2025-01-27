4bb19d38f569b361b589f379e04bd24fb77d2b2d
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-27 23:26:55 +0200

    Moduuli 6 tehtävä 5

---
+++ Moduuli 6/Tehtävä 5.py
@@ -0,0 +1,14 @@
+def lista(lista1):
+    print("parilliset")
+    for t in lista1:
+        print(t)
+    return
+lista1=[1,2,3,4,5,6,7,8,9,10]
+lista(lista1)
+print("parittomat")
+for o in range(1,10,2):
+    print(o)
+
+
+
+
