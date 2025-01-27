7ba52ffb7cbd03ec1d42c91ebeb5c67e85b59cff
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-27 23:37:17 +0200

    Moduuli 6 tehtävä 5

--- Moduuli 6/Tehtävä 5.py
+++ Moduuli 6/Tehtävä 5.py
@@ -1,14 +1,14 @@
 def lista(lista1):
-    print("parilliset")
+    print("Kokonaisluvut: ")
     for t in lista1:
         print(t)
-    return
-lista1=[1,2,3,4,5,6,7,8,9,10]
-lista(lista1)
-print("parittomat")
-for o in range(1,10,2):
-    print(o)
+    print("Parittomat luvut: ")
+    for t in range(1,15,2):
+        print(t)
 
+    return
+lista1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+lista(lista1)
 
 
 
