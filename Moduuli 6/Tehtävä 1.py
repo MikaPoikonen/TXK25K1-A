828c2bf10703191900af420777debafc52b46328
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-27 12:03:17 +0200

    Moduuli 6 tehtävä 1,2,3

--- Moduuli 6/Tehtävä 1.py
+++ Moduuli 6/Tehtävä 1.py
@@ -1,7 +1,3 @@
-#Kirjoita parametriton funktio, joka palauttaa paluuarvonaan
-##1..6. Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
-#Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
-from operator import truediv
 def nopat():
     print(noppa)
     return
