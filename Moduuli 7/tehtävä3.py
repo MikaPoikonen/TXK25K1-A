ce5d8ba60ae52f02164b5193a1375f7877f9e06f
Mika Poikonen <mikapoi@metropolia.fi>
2025-01-31 09:33:32 +0200

    Moduuli 7 tehtävä 3

--- Moduuli 7/tehtävä3.py
+++ Moduuli 7/tehtävä3.py
@@ -2,14 +2,17 @@ icao=set ()
 lentokentta_lisaa={}
 
 while True:
-    toiminto=input("Valitse toiminto: lisää kenttä, haku tai lopeta")
+    toiminto=input("Valitse toiminto: lisää, haku tai lopeta: ")
     if toiminto=="lopeta":
         break
     elif toiminto=="lisää":
-        koodi=input(print("Anna ICAO koodi:"))
-        kentta=input(print("Anna Kentän nimi"))
-        icao.add(icao)
-    else:
-        for i in icao:
-            print(i)
+        koodi=input(print("Anna ICAO koodi: "))
+        kentta=input(print("Anna Kentän nimi: "))
+        icao.add(koodi)
+        lentokentta_lisaa[koodi]=kentta
+    elif toiminto=="haku":
+        print(icao)
+        icao_haku = input("Anna haluamasi ICAO: ")
+        if icao_haku in lentokentta_lisaa:
+            print(f"{lentokentta_lisaa[icao_haku]}")
 
