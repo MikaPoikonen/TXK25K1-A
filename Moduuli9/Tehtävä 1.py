7603dd46ecb55e4a662497c973e1ce8c268d504d
Mika Poikonen <mikapoi@metropolia.fi>
2025-03-19 13:14:27 +0200

    Moduuli 9 tehtävä 1

--- Moduuli9/Tehtävä 1.py
+++ Moduuli9/Tehtävä 1.py
@@ -1,9 +1,11 @@
 class Auto:
     def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus = 0, kuljettu_matka = 0):
-        Auto.rekisteritunnus = rekisteritunnus
-        Auto.huippunopeus = huippunopeus
-        Auto.tämänhetkinen_nopeus = tämänhetkinen_nopeus
-        Auto.kuljettu_matka = kuljettu_matka
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = tämänhetkinen_nopeus = 0
+        self.kuljettu_matka = kuljettu_matka = 0
 
-Auto1 =Auto("ABC-123",142)
-print("Rekisteritunnus", Auto.rekisteritunnus, "Huippunopeus", Auto.huippunopeus, "Tämänhetkinen nopeus", Auto.tämänhetkinen_nopeus, "Kuljettu matka", Auto.kuljettu_matka)
+def main():
+    uusi_auto=Auto("ABC-123",142)
+    print("Rekisteritunnus", uusi_auto.rekisteritunnus,"Huippunopeus:",uusi_auto.huippunopeus,"mk/h")
+main()
