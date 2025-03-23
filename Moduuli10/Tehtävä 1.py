e46d8d2138f15801f68be754e5574a69b41ff878
Mika Poikonen <mikapoi@metropolia.fi>
2025-03-23 18:20:49 +0200

    Moduuli 10 tehtävä 1

---
+++ Moduuli10/Tehtävä 1.py
@@ -0,0 +1,28 @@
+class Hissi:
+    def __init__(self, ylin=5, alin = 0):
+        self.ylin = ylin
+        self.alin = alin
+        self.kerros = 0
+
+    def hissi_ylös(self):
+        if self.kerros < self.ylin:
+            self.kerros = self.kerros +1
+            print(self.kerros)
+
+    def hissi_alas(self):
+        if self.kerros > self.alin:
+            self.kerros = self.kerros -1
+            print(self.kerros)
+
+    def kerrokseen(self,haluttu_kerros):
+        while self.kerros != haluttu_kerros:
+            if self.kerros < haluttu_kerros:
+                self.hissi_ylös()
+            if self.kerros > haluttu_kerros:
+                self.hissi_alas()
+
+
+hissi1 =Hissi()
+print(hissi1.kerros)
+hissi1.kerrokseen(5)
+hissi1.kerrokseen(2)
